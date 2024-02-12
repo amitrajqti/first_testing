0ce00b44b4c42f0d4e97f85fb59b8fe1ce2b087e
amitraj <amitraj@qti.qualcomm.com>
2024-02-12 17:35:19 +0530

    Commint g my 3nd

--- first.cpp
+++ first.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main()
 {
     cout<<"Hey, we are here tring to setup CI/CD pipeline";
-    cout<<"Tracking changes here"
+    cout<<"Tracking changes here";
+    cout<<"This time we have auto mated the things";
     return 0;
 }
